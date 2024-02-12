33cf5e3ce0abbfa402cf2d7938e63df99611ca59
smdbs <1289707349@qq.com>
2024-02-12 02:16:52 -0500

    Added Meta text

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ import {
 function App() {
   const [cells, setCells] = useState<number[]>(Array(16).fill(0));
   const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
   const [gameState, setGameState] = useState(0); // 0: ready, 1: playing, 2: gameover
   const [animations, setAnimations] = useState<Animation[]>([]);
 
@@ -42,25 +43,26 @@ function App() {
         return;
       }
       let newCells, newScore, animations;
-      if (e.key === "ArrowLeft") {
+      if (e.key === "ArrowLeft" || e.key === "a") {
         ({ newCells, newScore, animations } = handleLeft(cells));
-      } else if (e.key === "ArrowRight") {
+      } else if (e.key === "ArrowRight" || e.key === "d") {
         ({ newCells, newScore, animations } = handleRight(cells));
-      } else if (e.key === "ArrowUp") {
+      } else if (e.key === "ArrowUp" || e.key === "w") {
         ({ newCells, newScore, animations } = handleUp(cells));
-      } else if (e.key === "ArrowDown") {
+      } else if (e.key === "ArrowDown" || e.key === "s") {
         ({ newCells, newScore, animations } = handleDown(cells));
       } else {
         return;
       }
       setCells(newCells);
       setScore(score + newScore);
+      setHighScore(Math.max(highScore, score + newScore));
       setAnimations(animations);
       if (isGameOver(newCells)) {
         setGameState(2);
       }
     },
-    [cells, score, gameState]
+    [gameState, score, highScore, cells]
   );
 
   useEffect(() => {
@@ -73,11 +75,33 @@ function App() {
   return (
     <div className="size-screen flex flex-col items-center gap-2 p-2">
       <h1 className="text-3xl font-bold">2048 Demo</h1>
-      <p className="text-2xl">Score: {score}</p>
+      <p className="mt-2 text-2xl">
+        <span>Score: {score}</span>
+        <span className="ml-6">High Score: {highScore}</span>
+      </p>
       <div
         className="relative grid grid-cols-4 grid-rows-4 size-2xl justify-around gap-6 rounded-lg p-6"
         style={{ background: "rgb(187, 173, 160)" }}
       >
+        {gameState !== 1 && (
+          <div className="absolute left-0 top-0 z-50 size-full flex items-center justify-center rounded-lg bg-gray-400 bg-opacity-60">
+            {gameState === 0 && (
+              <div className="flex flex-col items-center justify-center rounded-lg bg-orange-400 p-6">
+                <p className="text-3xl font-bold">Press "R" to start</p>
+                <p className="mt-2 text-xl font-bold">
+                  Use (↑ ↓ ← →) or WASD to move
+                </p>
+              </div>
+            )}
+            {gameState === 2 && (
+              <div className="flex flex-col items-center justify-center rounded-lg bg-gray-100 p-6 text-3xl font-bold">
+                <p className="text-red-500">Game Over</p>
+                <p className="mt-2">Press "R" to restart</p>
+              </div>
+            )}
+          </div>
+        )}
+
         {cells.map((val, i) => (
           <div className="relative">
             <div
@@ -116,7 +140,7 @@ function Cell(props: { val: number }) {
     to: {
       scale: 1,
     },
-    config: { mass: 5, tension: 300, friction: 5, clamp: true },
+    config: { mass: 5, tension: 280, friction: 5, clamp: true },
   });
 
   return (
